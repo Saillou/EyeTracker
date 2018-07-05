f78f94fd61f74099e00b184ff997b1d35bd634a1
Saillou <chauvinremy@upyourprod.fr>
2018-07-05 22:41:20 +0900

    Raspi commit too

---
+++ Sources/main_ubuntu.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#include "Dk/Protocole.hpp"
+#include "Dk/ManagerConnection.hpp"
+
+#define MAXPENDING 	5
+#define SOCKET_PORT 3000
+
+using namespace Protocole;
+
+
+void handleClient(int idClient, std::shared_ptr<Server> server, std::shared_ptr<cv::VideoCapture> ptrCap = nullptr) {
+	std::cout << "Handle new client" << std::endl;
+	BinMessage msg;
+	
+	// Define frame expected
+	cv::Mat frameCam = cv::Mat::zeros(1000, 1000, CV_8UC3);
+	size_t iFrameSend = 0;
+	
+	// Encodage declaration variables
+	std::vector<uchar> buf;
+	const std::vector<int> params{CV_IMWRITE_JPEG_QUALITY, 80};
+	const std::string format = ".jpg";
+	
+	// -- Handle client --
+	bool run = true;
+	while(run) {
+		// Get the frame
+		if(ptrCap == nullptr) {
+			frameCam = cv::Mat::zeros(frameCam.rows, frameCam.cols, frameCam.type());
+			cv::circle(frameCam, cv::Point(frameCam.cols/2, frameCam.rows/2), 0.25*frameCam.rows*(1+std::cos(0.1*iFrameSend)), cv::Scalar(255), -1, CV_AA);
+			if(frameCam.channels() > 1)
+				cv::cvtColor(frameCam, frameCam, cv::COLOR_BGR2GRAY);
+		}
+		else {
+			*ptrCap >> frameCam;
+			cv::cvtColor(frameCam, frameCam, cv::COLOR_BGR2GRAY);
+		}
+		
+		// Received
+		server->read(msg, idClient);
+		
+		// Answer
+		if(msg.isValide()) {
+			switch(msg.getAction()) {
+				// Client quit
+				case BIN_QUIT:
+				{ 
+					run = false;
+				}
+				break;
+				
+				// Frame info asked
+				case BIN_INFO:
+				{ 
+					CmdMessage cmd;
+					cmd.addCommand(CMD_HEIGHT, 	std::to_string(frameCam.rows));
+					cmd.addCommand(CMD_WIDTH, 	std::to_string(frameCam.cols));
+					cmd.addCommand(CMD_CHANNEL, std::to_string(frameCam.channels()));
+					
+					msg.set(BIN_MCMD, Message::To_string(cmd.serialize()));
+					server->write(msg, idClient);
+				}
+				break;
+				
+				// Send a frame
+				case BIN_GAZO: 
+				{	
+					cv::imencode(format, frameCam, buf, params);
+					msg.set(BIN_GAZO, buf.size(), (const char*)buf.data());
+					server->write(msg, idClient);
+
+					iFrameSend++;					
+				}
+				break;
+			}
+
+		}
+	}
+	server->closeSocket(idClient);
+	std::cout << "Client disconnected." << std::endl;
+}
+
+
+int main() {
+	// Try to open the cam
+	std::shared_ptr<cv::VideoCapture> pCap = std::make_shared<cv::VideoCapture>(0);
+	if(!pCap->isOpened())
+		pCap.reset();
+	
+	// Create server TCP
+	ManagerConnection managerConnection;
+	managerConnection.initialize();
+	auto server = managerConnection.createServer(SOCKET_PORT, MAXPENDING);
+
+	// Handle client until sigint
+	std::cout << "Wait for clients" << std::endl;
+	while(1) {
+		// Wait until client pop
+		handleClient(server->waitClient(), server, pCap);
+	}
+	
+	return 0;	
+}
+
---
+++ compile.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+# Functions
+create() {
+	if [ ! -d $1 ]
+	then
+		mkdir $1
+	fi	
+}
+cleanup() {
+	if [ -f $1 ]
+	then
+		rm $1
+	fi	
+}
+makePathSources() {
+	# Sources
+	for source in "${Sources[@]}"
+	do
+		if [ -f "../Sources/$source.cpp" ]
+		then
+			PathSources="$PathSources ../Sources/$source.cpp"
+		else
+			echo "$source.cpp is missing"
+			return 0
+		fi
+	done
+	
+	# Continue ? 	
+	if [ $compileDk == 0 ]
+		then return 1
+	fi
+	
+	# Sources of Lib Dk
+	for source in "${Dk[@]}"
+	do
+		if [ -f "../Sources/Dk/$source.cpp" ]
+		then
+			PathSources="$PathSources ../Sources/Dk/$source.cpp"
+		else
+			echo "Dk/$source.cpp is missing"
+			return 0
+		fi
+	done 
+	
+	return 1
+}
+makePathObjects() {
+	# Objects from source
+	for source in "${Sources[@]}"
+	do
+		if [ -f "../Objects/$source.o" ]
+		then
+			PathObjects="$PathObjects ../Objects/$source.o"
+		else
+			echo "$source.o is missing"
+			return 0
+		fi
+	done 
+	
+	# Objects from Dk
+	for source in "${Dk[@]}"
+	do
+		if [ -f "../Objects/$source.o" ]
+		then
+			PathObjects="$PathObjects ../Objects/$source.o"
+		else
+			echo "$source.o is missing"
+			return 0
+		fi
+	done 
+	
+	return 1	
+}
+
+
+# Define environnement
+create "Objects"
+create "Release"
+
+OutputName="EyeTracker"
+declare -a Sources=(
+	"main" 
+)
+declare -a Dk=(
+	"ManagerConnection" 
+	"Protocole" 
+	"Server" 
+	"Socket"
+)
+compileDk=1
+
+PathSources=""
+PathObjects=""
+
+# Compile source
+cd "Objects"
+cleanup *.o
+makePathSources
+if [ $? == 0 ]
+	then exit -1
+fi
+
+echo "Compile sources"
+g++ -c -std=gnu++11 -O2 \
+	-Wall \
+	$PathSources \
+	-I/usr/Local/include/ 
+cd ../
+
+
+# Link objects
+cd "Release"
+cleanup $OutputName 
+makePathObjects
+if [ $? == 0 ]
+	then exit -1
+fi
+
+echo "Link objects"
+g++ -o $OutputName \
+	$PathObjects \
+	-L/usr/Local/lib/ \
+	-lopencv_core \
+	-lopencv_highgui \
+	-lopencv_imgproc \
+	-lopencv_imgcodecs \
+	-lopencv_videoio \
+	
+cd ../
+
+
+# Launch
+echo "-----------"
+./Release/$OutputName
+
+exit 0
