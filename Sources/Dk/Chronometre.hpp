72aba17e74d0ad458e4ec4b7a631665cd4a81993
Saillou <chauvinremy@upyourprod.fr>
2018-07-19 14:19:20 +0900

    Add time helper

---
+++ Sources/Dk/Chronometre.hpp
@@ -0,0 +1,58 @@
+#ifndef CHRONOMETRE_H
+#define CHRONOMETRE_H
+
+#include <chrono>
+
+struct Chronometre {	
+public:
+	Chronometre():
+		_tRef(_now()),
+		_tBeg(_tRef),
+		_tEnd(_tRef)
+	{
+	}
+
+	// Methods	
+	void beg() {
+		_tBeg = _now();
+	}
+	void end() {
+		_tEnd = _now();
+	}
+	void reset() {
+		_tRef = _now();
+		_tBeg = _tRef;
+		_tEnd = _tRef;
+	}
+	
+	double clock_ms() const {
+		return  _diffMs(_tRef, _now());
+	}
+	double ms() const {
+		return _diffMs(_tBeg, _tEnd);
+	}
+	
+	// Static methods
+	static void wait(int ms) {
+		_timePoint c0 = _now();
+		while(_diffMs(c0, _now()) < ms);
+	}
+	
+private:
+	typedef std::chrono::high_resolution_clock::time_point 	_timePoint;
+	
+	// Static methods
+	static _timePoint _now() {
+		return std::chrono::high_resolution_clock::now();
+	}
+	static double _diffMs(const _timePoint& tA, const _timePoint& tB) {
+		return 1000*std::chrono::duration_cast<std::chrono::duration<double>>(tB - tA).count();
+	}
+	
+	// Members
+	_timePoint _tRef; // Creation of the chrono
+	_timePoint _tBeg;
+	_timePoint _tEnd;
+};
+
+#endif
